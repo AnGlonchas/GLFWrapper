237c8354d460b1a819365c46879a9200bf429abf
angl <jjose.preciado@hotmail.com>
2025-05-07 01:17:14 -0600

    Added various functions

--- glfwrapper.h
+++ glfwrapper.h
@@ -2,8 +2,21 @@
 
 #define GLFW_INCLUDE_NONE
 #include "include/GLFW/glfw3.h"
+#include <GL/gl.h>
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
 #include <stdlib.h>
+
+#ifndef PI
+    #define PI 3.14159265358979323846f
+#endif
+#ifndef DEG2RAD
+    #define DEG2RAD (PI/180.0f)
+#endif
+#ifndef RAD2DEG
+    #define RAD2DEG (180.0f/PI)
+#endif
 
 typedef struct {
     char *vertex;
@@ -27,8 +40,128 @@ typedef struct {
     float z;
 } Vector3;
 
+typedef struct {
+    int fps;
+    float deltatime;
+} DeltaTime;
+
 Shader LoadShader(const char *vertexPath, const char *fragPath) {
-    // To make
+    // To do
+}
+
+/* 
+static means its only for this file
+long double has the highest precission
+and its treated as a float
+
+*/
+
+static clock_t start;
+static clock_t stop;
+static double delta;
+
+
+/*
+Drawing Modes for Opengl
+
+see https://learn.microsoft.com/es-es/windows/win32/opengl/glbegin for more info
+
+*/
+void takeTime() {
+    start = clock();
+}
+
+void getTime() {
+    stop = clock();
+    delta = (double)(stop-start);
+}
+
+double getFrameTime() {
+    return delta;
+}
+
+void setBackgroundColor(float r, float g, float b) {
+    glClearColor(r,g,b,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void setBackgroundColorAlpha(float r, float g, float b, float a) {
+    glClearColor(r,g,b,a);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void drawLine(float x1, float y1, float x2, float y2) {
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+    glEnd();
+}
+
+void drawRectangle(float x, float y, float w, float h) {
+    glBegin(GL_TRIANGLES);
+
+    glVertex2f(x  ,y  );
+    glVertex2f(x  ,y+h);
+    glVertex2f(x+w,y+h);
+
+    glVertex2f(x  ,y  );
+    glVertex2f(x+w,y  );
+    glVertex2f(x+w,y+h);
+
+    glEnd();
+}
+
+void drawRectangleLines(float x, float y, float w, float h) {
+    glBegin(GL_LINES);
+
+    glVertex2f(x  ,y  );
+    glVertex2f(x+w,y  );
+
+    glVertex2f(x+w,y  );
+    glVertex2f(x+w,y+h);
+
+    glVertex2f(x+w,y+h);
+    glVertex2f(x  ,y+h);
+
+    glVertex2f(x  ,y+h);
+    glVertex2f(x  ,y  );
+
+    glEnd();
+}
+
+void drawPolygonLines(float centerx, float centery, float size, int sides) {
+    glBegin(GL_LINES);
+
+    for(int i = 0; i < sides; i++) {
+        glVertex2f(
+            centerx + size*cos(i*DEG2RAD*( 360/sides) ),
+            centery - size*sin(i*DEG2RAD*( 360/sides) )
+        );
+        glVertex2f(
+            centerx + size*cos((i+1)*DEG2RAD*( 360/sides) ),
+            centery - size*sin((i+1)*DEG2RAD*( 360/sides) )
+        );
+    }
+    glEnd();
+
+}
+
+void drawPolygon(float centerx, float centery, float size, int sides) {
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(centerx, centery);
+
+    for(int i = 0; i < sides; i++) {
+        glVertex2f(
+            centerx + size*cos(i*DEG2RAD*( 360/sides) ),
+            centery - size*sin(i*DEG2RAD*( 360/sides) )
+        );
+        glVertex2f(
+            centerx + size*cos((i+1)*DEG2RAD*( 360/sides) ),
+            centery - size*sin((i+1)*DEG2RAD*( 360/sides) )
+        );
+    }
+    glEnd();
+
 }
 
 
--- main.c
+++ main.c
@@ -4,6 +4,13 @@ This file is only for testing
 the glfwrapper.h is the product
 feel free to edit this file
 
+Notes:
+
+The screen is like a cartesian plane
+the left side is x = -1
+the right side is x = 1
+same for y
+
 */
 
 #define GLFW_INCLUDE_NONE
@@ -17,39 +24,37 @@ int main()
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    // Initialize the library
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello triangle", NULL, NULL);
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(640, 640, "Test file", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-
-        glEnd();
+        // Render here
+        setBackgroundColor(0.0f,0.2f,0.5f);
 
 
-        /* Swap front and back buffers */
+        drawPolygonLines(0.0f, 0.0f, 0.1f, 8);
+        //drawRectangle(0.0f, 0.0f, 0.5, 0.5f);
+        drawRectangleLines(0.0f, 0.0f, 0.5, 0.5f);
+        //drawLine(-0.5f, -0.5f, 0.5f, 0.5f);
+
+
+        // Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events
         glfwPollEvents();
     }
 
